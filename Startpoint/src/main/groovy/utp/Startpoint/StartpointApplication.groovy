a95b7b646cd28c58241a144e2f1a104823a60776
jose-quio <114790377+jose-quio@users.noreply.github.com>
2026-05-20 22:56:08 -0500

    new proyect

---
+++ Startpoint/src/main/groovy/utp/Startpoint/StartpointApplication.groovy
@@ -0,0 +1,13 @@
+package utp.Startpoint
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class StartpointApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(StartpointApplication, args)
+	}
+
+}
