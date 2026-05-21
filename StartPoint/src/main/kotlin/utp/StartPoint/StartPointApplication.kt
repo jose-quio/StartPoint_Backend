7eee4dac3f541ed6f0295339a5cd9ea8d152a99a
jose-quio <114790377+jose-quio@users.noreply.github.com>
2026-05-20 22:49:30 -0500

    created proyect

---
+++ StartPoint/src/main/kotlin/utp/StartPoint/StartPointApplication.kt
@@ -0,0 +1,11 @@
+package utp.StartPoint
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StartPointApplication
+
+fun main(args: Array<String>) {
+	runApplication<StartPointApplication>(*args)
+}
---
+++ StartPoint/src/test/kotlin/utp/StartPoint/StartPointApplicationTests.kt
@@ -0,0 +1,13 @@
+package utp.StartPoint
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartPointApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
